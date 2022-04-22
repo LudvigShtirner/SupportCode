1069dc0f857518b382c68970708f7ce0ef6ef72c
Filippov Alex <filippovalex24@mail.ru>
2022-04-22 11:13:50 +0700

    rename to support code

--- Sources/SupportCode/SupportCode.swift
+++ Sources/SupportCode/SupportCode.swift
@@ -1,4 +1,4 @@
-public struct SupportCodePackage {
+public struct SupportCode {
     public private(set) var text = "Hello, World!"
 
     public init() {
--- Tests/SupportCodeTests/SupportCodeTests.swift
+++ Tests/SupportCodeTests/SupportCodeTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import SupportCodePackage
+@testable import SupportCode
 
-final class SupportCodePackageTests: XCTestCase {
+final class SupportCodeTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SupportCodePackage().text, "Hello, World!")
+        XCTAssertEqual(SupportCode().text, "Hello, World!")
     }
 }
